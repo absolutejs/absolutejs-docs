37524e2975edf0d41d46839f03be398af917b7cb
Alex Kahn <alexkahndev@gmail.com>
2025-06-09 02:23:44 -0400

    feat: Enhance useThemeColors hook to include theme animation effects with useSpring

--- src/frontend/hooks/useThemeColors.ts
+++ src/frontend/hooks/useThemeColors.ts
@@ -8,11 +8,12 @@ import {
 	lightTertiaryColor
 } from '../styles/colors';
 import { useThemeStore } from './useThemeStore';
+import { useEffect } from 'react';
 
 export const useThemeColors = () => {
 	const theme = useThemeStore((state) => state.theme);
 
-	const [themeSprings] = useSpring(() => ({
+	const [themeSprings, themeApi] = useSpring(() => ({
 		contrastPrimary:
 			theme === 'dark' ? lightPrimaryColor : darkPrimaryColor,
 		contrastSecondary:
@@ -22,6 +23,21 @@ export const useThemeColors = () => {
 			theme === 'dark' ? darkSecondaryColor : lightSecondaryColor,
 		themeTertiary: theme === 'dark' ? darkTertiaryColor : lightTertiaryColor
 	}));
+
+	useEffect(() => {
+		themeApi.start({
+			contrastPrimary:
+				theme === 'dark' ? lightPrimaryColor : darkPrimaryColor,
+			contrastSecondary:
+				theme === 'dark' ? lightSecondaryColor : darkSecondaryColor,
+			themePrimary:
+				theme === 'dark' ? darkPrimaryColor : lightPrimaryColor,
+			themeSecondary:
+				theme === 'dark' ? darkSecondaryColor : lightSecondaryColor,
+			themeTertiary:
+				theme === 'dark' ? darkTertiaryColor : lightTertiaryColor
+		});
+	}, [theme, themeApi]);
 
 	return themeSprings;
 };
