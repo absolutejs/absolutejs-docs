fe596fd4467ba4ef3debadacb5a8476e623a8761
Alex Kahn <alexkahndev@gmail.com>
2025-06-11 19:37:04 -0400

    feat: Refactor CreateButton component to improve mobile responsiveness; adjust font size based on screen size

--- src/frontend/components/home/CreateButton.tsx
+++ src/frontend/components/home/CreateButton.tsx
@@ -1,28 +1,35 @@
 import { animated } from '@react-spring/web';
 import { ThemeProps } from '../../../types/types';
 import { CopyButton } from '../utils/CopyButton';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
-export const CreateButton = ({ themeSprings }: ThemeProps) => (
-	<animated.div
-		style={{
-			alignItems: 'center',
-			backgroundColor: themeSprings.themeTertiary,
-			borderRadius: '16px',
-			boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-			color: themeSprings.contrastPrimary,
-			display: 'flex',
-			justifyContent: 'center',
-			padding: '10px 20px'
-		}}
-	>
-		<code
+export const CreateButton = ({ themeSprings }: ThemeProps) => {
+	const { isSizeOrLess } = useMediaQuery();
+	const isMobile = isSizeOrLess('sm');
+
+	return (
+		<animated.div
 			style={{
-				fontSize: '1.2rem',
-				marginRight: '2rem'
+				alignItems: 'center',
+				backgroundColor: themeSprings.themeTertiary,
+				borderRadius: '16px',
+				boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+				color: themeSprings.contrastPrimary,
+				display: 'flex',
+				justifyContent: 'center',
+				padding: '10px 20px'
 			}}
 		>
-			bun create absolutejs
-		</code>
-		<CopyButton text="bun create absolutejs" />
-	</animated.div>
-);
+			<code
+				style={{
+					fontSize: isMobile ? '1rem' : '1.2rem',
+					textWrap: 'nowrap',
+					marginRight: '2rem'
+				}}
+			>
+				bun create absolutejs
+			</code>
+			<CopyButton text="bun create absolutejs" />
+		</animated.div>
+	);
+};
